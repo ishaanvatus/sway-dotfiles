368d049ee5ffcc163bcd5b1dfbd3497304aa0d7f
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2026-03-09 00:07:23 +0530

    add: added line to dnf.conf
    max_parallel_downloads=6

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,14 @@
 
 mkdir -p ~/documents ~/downloads ~/music ~/pictures ~/videos ~/.config/xfce4 ~/.software ~/src ~/.local/bin ~/.local/share/fonts/Mononoki
 cp Mononoki/* ~/.local/share/fonts/Mononoki/
+
+echo "max_parallel_downloads=6" | sudo tee -a  /etc/dnf/dnf.conf
+sudo dnf -y upgrade --refresh
+
 sudo dnf -y install stow
 rm ~/.bashrc ~/.bash_profile
 stow .
+
 source ~/.bashrc
 bash rpmfusion.sh
 bash packages.sh
