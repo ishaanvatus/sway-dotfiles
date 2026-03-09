317f5f262d20d15ebfcf224f6ee6a003036a53cb
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2026-03-10 04:41:35 +0530

    fix: deno bash completion updating
    how can you wear shoes if you don't know where they are

--- install.sh
+++ install.sh
@@ -47,6 +47,7 @@ echo "don't add anything to path for rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo "don't add anything to path or enable completions for deno"
 curl -fsSL https://deno.land/install.sh | sh
+source ~/.bashrc
 deno completions bash > ~/.bashrc.d/user/deno.bash
 
 ## flatpaks
