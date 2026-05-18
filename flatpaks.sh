dc323a7c8bfb19c8eead21e5511a0806c5ba646e
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2026-05-18 06:33:25 +0530

    なにやってもうまくいかない

--- flatpaks.sh
+++ flatpaks.sh
@@ -4,7 +4,4 @@ flatpak -y install flathub \
     com.obsproject.Studio \
     org.gnome.Epiphany \
     org.signal.Signal \
-    org.kde.kdenlive \
-    org.kde.krita \
-    org.mozilla.Thunderbird \
     io.github.ciromattia.kcc
--- toolchains.sh
+++ toolchains.sh
@@ -6,3 +6,5 @@ echo "don't add anything to path or enable completions for deno"
 curl -fsSL https://deno.land/install.sh | sh
 . ~/.bashrc
 deno completions bash > ~/.bashrc.d/user/deno.bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+echo "remove lines added to .bashrc by nvm"
