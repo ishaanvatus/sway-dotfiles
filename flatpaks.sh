eec442a6b86f7f4458ef24411af62ed3161516ad
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2026-06-28 00:23:42 +0530

    looping the rooms arc 6-7

--- flatpaks.sh
+++ flatpaks.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-flatpak -y install flathub \
+flatpak install flathub \
     com.obsproject.Studio \
     org.gnome.Epiphany \
     org.signal.Signal \
     io.github.ciromattia.kcc \
-    flatpak run org.mozilla.thunderbird_esr
+    org.mozilla.thunderbird_esr
